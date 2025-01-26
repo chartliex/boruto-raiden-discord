28287342e8341dd4ea8ba7e5f10a46f3132f6f34
chartliex <plaxv17@gmail.com>
2025-01-26 00:29:19 -0300

    Feature: Adicionado Dockerfile e Bun

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use uma imagem base do Node.js
+FROM bun:1.2.0
+
+# Defina o diretório de trabalho dentro do contêiner
+WORKDIR /app
+
+# Copie os arquivos package.json e package-lock.json
+COPY package*.json ./
+
+# Instale as dependências
+RUN bun install
+
+# Copie todo o restante do código do aplicativo
+COPY . .
+
+# Comando para iniciar o aplicativo
+CMD ["bun", "start"]
--- mongodb.js
+++ mongodb.js
@@ -1,30 +1,40 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const mongoclient = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true,
-  },
+    deprecationErrors: true
+  }
 });
 
-let userDB, infoGameDB, jutsuDB, itemDB, invDB, clanDB;
+let userDB, infoGameDB, jutsuDB, itemDB, invDB, clanDB, ninjaTalentsDB;
 
 async function connectToMongoDB() {
   try {
     await mongoclient.connect();
-    const db = mongoclient.db('borutoraiden');
-    userDB = db.collection('users');
-    infoGameDB = db.collection('infoGame');
-    jutsuDB = db.collection('jutsus');
-    itemDB = db.collection('items');
-    invDB = db.collection('invs');
-    clanDB = db.collection('clans');
+    const db = mongoclient.db("borutoraiden");
+    userDB = db.collection("users");
+    infoGameDB = db.collection("infoGame");
+    jutsuDB = db.collection("jutsus");
+    itemDB = db.collection("items");
+    invDB = db.collection("invs");
+    clanDB = db.collection("clans");
+    ninjaTalentsDB = db.collection("ninjaTalents");
   } catch (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error("Erro ao conectar ao banco de dados:", err);
   }
 }
 
 connectToMongoDB();
 
-export { userDB, infoGameDB, jutsuDB, itemDB, invDB, clanDB};
+export {
+  connectToMongoDB,
+  userDB,
+  infoGameDB,
+  jutsuDB,
+  itemDB,
+  invDB,
+  clanDB,
+  ninjaTalentsDB
+};
