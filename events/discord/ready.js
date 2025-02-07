e93f23a5c0e362df5963d27e76abd35a9b0197bf
chartliex <plaxv17@gmail.com>
2025-02-07 01:21:03 -0300

    agora ela manda uma mensagem no ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,6 +1,18 @@
-export const name = "ready"
+export const name = "ready";
 export const once = true;
+
 export async function execute(client) {
+    const channelId = "1294752951637770291";
+
+    // Define o status do bot
     await client.user.setActivity(`Boruto Raiden - Temporada 1`);
     console.log(`Online - ${client.user.tag}`);
+
+    // Obtém o canal e envia a mensagem
+    const channel = await client.channels.fetch(channelId).catch(console.error);
+    if (channel && channel.isTextBased()) {
+        channel.send("Ah... estou aqui. Tente não me entediar.");
+    } else {
+        console.error(`Não foi possível encontrar o canal de ID ${channelId}.`);
+    }
 }
